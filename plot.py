a4762f7ab176c041ecdc3f1c0941a6ab39be5850
matti <dorsch@uni-potsdam.de>
2025-09-07 14:31:47 +0200

    group stars based on coords

--- plot.py
+++ plot.py
@@ -4,15 +4,97 @@ import pandas as pd
 import numpy as np
 from glob import glob
 from astropy.io import fits
+from astropy.coordinates import SkyCoord
+import astropy.units as u
 
-def read_fits_file(filepath):
+def group_close_stars_with_bjd(fps, max_sep_arcsec=30):
+    """
+    Read RA/Dec and BJD from a list of FITS files and group stars closer than max_sep_arcsec.
+    
+    Parameters
+    ----------
+    fps : list of str
+        List of FITS file paths.
+    max_sep_arcsec : float
+        Maximum separation in arcseconds to consider stars as a group.
+    
+    Returns
+    -------
+    groups : list of dict
+        Each dict has keys:
+            'files' : list of FITS file paths in the group
+            'bjds'  : list of BJD values corresponding to the files
+    coords : SkyCoord
+        SkyCoord object of all stars.
+    """
+    coords_list = []
+    files_list = []
+    bjds_list = []
+
+    # Collect RA/Dec and BJD for all FITS files
+    for fp in fps:
+        if "fits" in fp:
+            header, bjd = read_fits_header(fp)
+            if "RA" in header and "DEC" in header:
+                ra_deg = header["RA"]
+                dec_deg = header["DEC"]
+                coords_list.append((ra_deg, dec_deg))
+                files_list.append(fp)
+                bjds_list.append(bjd)
+
+    if not coords_list:
+        return [], None
+
+    # Convert to SkyCoord array
+    coords = SkyCoord(
+        ra=[c[0] for c in coords_list]*u.deg,
+        dec=[c[1] for c in coords_list]*u.deg
+    )
+
+    # Search for pairs within max_sep
+    max_sep = max_sep_arcsec * u.arcsec
+    idx1, idx2, _, _ = coords.search_around_sky(coords, max_sep)
+
+    # Group stars (avoid self-matches and duplicates)
+    groups = []
+    used = set()
+    for i1, i2 in zip(idx1, idx2):
+        if i1 == i2:
+            continue  # skip self
+        if i1 in used or i2 in used:
+            continue
+        group_files = [files_list[i1], files_list[i2]]
+        group_bjds = [bjds_list[i1], bjds_list[i2]]
+        groups.append({'files': group_files, 'bjds': group_bjds})
+        used.update([i1, i2])
+    groups = [pd.DataFrame(g) for g in groups]
+
+    return groups, coords
+
+def read_fits_header(filepath):
+    """Read FITS header and convert RA/DEC to decimal degrees if present."""
     with fits.open(filepath) as hdul:
         header = hdul[0].header
-        table_data = hdul['SCIENCE'].data
+        ra = header.get('RA', None)
+        dec = header.get('DEC', None)
+
+        if ra is not None and dec is not None:
+            # Convert RA/DEC to decimal degrees
+            coord = SkyCoord(ra=ra, dec=dec, unit=(u.hourangle, u.deg))
+            header["RA"] = np.float64(coord.ra.deg)
+            header["DEC"] = np.float64(coord.dec.deg)
+        else:
+            print("RA or DEC not found in header.")
+
         bjd = header.get('BJD', None)
+        return header, bjd
+
+def read_fits_data(filepath):
+    """Read FITS 'SCIENCE' table data and convert to pandas DataFrame."""
+    with fits.open(filepath) as hdul:
+        table_data = hdul['SCIENCE'].data
         data_dict = {}
-        if bjd is not None:
-            data_dict['bjd'] = bjd
+
         for col in table_data.columns:
             col_data = table_data[col.name]
             if hasattr(col_data, 'byteswap'):
@@ -22,8 +104,9 @@ def read_fits_file(filepath):
                 else:
                     col_data = col_data.view(col_data.dtype.newbyteorder())
             data_dict[col.name] = col_data
+
         df = pd.DataFrame(data_dict)
-        return df, header
+        return df
 
 def read_spectrum_file(fp):
     df = pd.read_csv(fp, sep="\\s+", header=None)
@@ -55,15 +138,18 @@ def main():
         fps = list(glob(fps))
     else:
         fps = [fps]
-
     print(fps)
+
+    groups, coords = group_close_stars_with_bjd(fps, max_sep_arcsec=30)
+    for g in groups: print(g)
 
     for fp in fps:
         if "fits" in fp:
-            data, header = read_fits_file(fp)
-            wave = data["wave"]
-            flux = data["flux"]
-            err = data["error"]
+            header, bjd = read_fits_header(fp)
+            df = read_fits_data(fp)
+            wave = df["wave"]
+            flux = df["flux"]
+            err = df["error"]
         else:
             wave, flux, err = read_spectrum_file(fp)
         snr = flux/err
